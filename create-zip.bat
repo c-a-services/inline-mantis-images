ad9e55147b320bdc64f921b4ca6004f4ea36056f
Stefan CORDES <dt0b35@canda.com>
2017-08-29 13:09:17 +0200

    https://chrome.google.com/webstore/developer/dashboard

--- create-zip.bat
+++ create-zip.bat
@@ -2,3 +2,6 @@ rem see https://developer.chrome.com/webstore/get_started_simple#step5
 
 del google-chrome-inline-images.zip
 "c:\Program Files\7-Zip\7z.exe" a google-chrome-inline-images.zip *.png *.json *.js
+
+rem Now go to https://chrome.google.com/webstore/developer/dashboard
+rem and publish the Zip
